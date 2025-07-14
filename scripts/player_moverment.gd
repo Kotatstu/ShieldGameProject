6b7de7f884e672a41b2103dbec41088d69789a6a
Kotatstu <kotatstu0303@gmail.com>
2025-07-14 15:46:24 +0700

    Revert "Merge pull request #1 from tan030104/tan"
    This reverts commit cf88d14256ab40808b1cf32d8d520a3e816c3bbc, reversing
    changes made to a8523ed5b942daefce99c3ebfbba392fbebce428.

--- scripts/player_moverment.gd
+++ scripts/player_moverment.gd
@@ -14,8 +14,7 @@ func _process(delta: float) -> void:
 	
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-	
+	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
